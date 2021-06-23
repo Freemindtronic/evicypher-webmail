cc9f3ba5f9505e188093d82e4ffd308ac829d853
Gautier Ben Aim <benaim.gautier@gmail.com>
2021-06-23 11:16:32 +0200

    style: fixed comments

--- rollup.config.js
+++ rollup.config.js
@@ -44,11 +44,11 @@ export default [
       svelte({
         preprocess: sveltePreprocess({ sourceMap: !production }),
         compilerOptions: {
-          // enable run-time checks when not in production
+          // Enable run-time checks when not in production
           dev: !production,
         },
       }),
-      // we'll extract any component CSS out into
+      // We'll extract any component CSS out into
       // a separate file - better for performance
       css({ output: 'popup.css' }),
 
