318560c449de32ae72f65e9f31a245684ddaedc3
GauBen <benaim.gautier@gmail.com>
2021-06-28 09:03:11 +0200

    docs: added docblocks

--- src/legacy-code/zeroconf-service.ts
+++ src/legacy-code/zeroconf-service.ts
@@ -1,18 +1,31 @@
 import { browser, Runtime } from 'webextension-polyfill-ts'
 
+const APPLICATION_ID = 'com.freemindtronic.evidns'
+
+/** A response object, sent by EviDNS v1. */
 export interface ZeroconfResponse {
+  /** Name of the service. */
+  source: string
+  /** Version of the service. */
+  version: number
+  /** List of devices found. */
   result: Array<{
-    a: string
+    /** Unique identifier (UUIDv4). */
     name: string
+    /** IP address. */
+    a: string
+    /** Reachable port. */
     port: number
+    /** Hostname. */
     target: string
-    txt: string[]
+    /** An HTTP address produced with IP and port. */
     url: string
+    /** Additional information. */
+    txt: string[] | null
   }>
 }
 
-const APPLICATION_ID = 'com.freemindtronic.evidns'
-
+/** @returns Whether the Zeroconf service is properly installed. */
 export const isZeroconfServiceInstalled = async (): Promise<boolean> => {
   try {
     await browser.runtime.sendNativeMessage(APPLICATION_ID, { cmd: 'Version' })
@@ -22,5 +35,6 @@ export const isZeroconfServiceInstalled = async (): Promise<boolean> => {
   }
 }
 
+/** Open a connection to the Zeroconf service. */
 export const getZeroconfService = (): Runtime.Port =>
   browser.runtime.connectNative(APPLICATION_ID)
