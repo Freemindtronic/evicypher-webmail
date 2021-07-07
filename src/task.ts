a594565773110df33e1330d9cef55be4d1303ece
Gautier Ben Aim <benaim.gautier@gmail.com>
2021-07-07 13:44:56 +0200

    style: added documentation

--- src/task.ts
+++ src/task.ts
@@ -4,12 +4,73 @@ import { pair } from 'background/tasks/pair'
 import type { ReportDetails, Reporter, ReporterImpl, StateKey } from 'report'
 import { browser } from 'webextension-polyfill-ts'
 
+/**
+ * A background task is an asynchronous generator transparently connected to a
+ * foreground task. The `yield` keyword is used to exchange data between them.
+ *
+ * @typeParam TInitialValue - Type of the data required to start the task
+ * @typeParam TYielded - Type of the data sent to the foreground task
+ * @typeParam TReturn - Type of the data used as a return value to
+ *   {@link runBackgroundTask}
+ * @typeParam TNext - Type of the data received from the foreground task
+ */
+export type BackgroundTask<TInitialValue, TYielded, TReturn, TNext> = (
+  initialValue: TInitialValue,
+  reporter: Reporter,
+  signal: AbortSignal
+) => AsyncGenerator<TYielded, TReturn, TNext>
+
+/**
+ * This type swaps `TYielded` and `TNext` of the `AsyncGenerator` produced by
+ * the {@link BackgroundTask}.
+ */
+export type ForegroundTask<T> = T extends BackgroundTask<
+  never,
+  infer U,
+  unknown,
+  infer V
+>
+  ? () => AsyncGenerator<V, void, U>
+  : never
+
+/** Retrieve the initial value from a background task. */
+export type InitialValue<T> = T extends BackgroundTask<
+  infer U,
+  unknown,
+  unknown,
+  unknown
+>
+  ? U
+  : never
+
+/** Retrieve the return value from a background task. */
+export type ReturnValue<T> = T extends BackgroundTask<
+  never,
+  unknown,
+  infer U,
+  unknown
+>
+  ? U
+  : never
+
+/**
+ * A report message is a message from the back to the front containing
+ * {@link ReportDetails | useful details} on the progression of a task.
+ */
 export type ReportMessage<T extends StateKey> = {
   type: 'report'
   state: T
   details: T extends keyof ReportDetails ? ReportDetails[T] : undefined
 }
 
+/**
+ * A message sent by the background task to the front end. It may be:
+ *
+ * - A `request`: the request content is returned by the `yield` keyword in the
+ *   front end generator;
+ * - A `result`: the final result as returned by {@link runBackgroundTask};
+ * - A `report`: {@link ReportMessage}.
+ */
 export type MessageFromBackToFront<T> = T extends BackgroundTask<
   never,
   infer U,
@@ -28,6 +89,12 @@ export type MessageFromBackToFront<T> = T extends BackgroundTask<
       | ReportMessage<StateKey>
   : never
 
+/**
+ * A message sent by the front end to the background task. It may be:
+ *
+ * - A `response` to a `request`;
+ * - An `abort` request, that will cause the background task to be aborted.
+ */
 export type MessageFromFrontToBack<T> = T extends BackgroundTask<
   never,
   unknown,
@@ -42,54 +109,31 @@ export type MessageFromFrontToBack<T> = T extends BackgroundTask<
       | { type: 'abort' }
   : never
 
-/** A background task is an asynchronous generator piped with a foreground task. */
-export type BackgroundTask<TInitialValue, TYielded, TReturn, TNext> = (
-  initialValue: TInitialValue,
-  reporter: Reporter,
-  signal: AbortSignal
-) => AsyncGenerator<TYielded, TReturn, TNext>
-
-export type ForegroundTask<T> = T extends BackgroundTask<
-  never,
-  infer U,
-  unknown,
-  infer V
->
-  ? () => AsyncGenerator<V, void, U>
-  : never
-
-/** Retreive the initial value from a background task. */
-export type InitialValue<T> = T extends BackgroundTask<
-  infer U,
-  unknown,
-  unknown,
-  unknown
->
-  ? U
-  : never
-
-/** Retreive the return value from a background task. */
-export type ReturnValue<T> = T extends BackgroundTask<
-  never,
-  unknown,
-  infer U,
-  unknown
->
-  ? U
-  : never
-
+/** All the tasks available. */
 export const Task = {
   ENCRYPT: 'encrypt',
   DECRYPT: 'decrypt',
   PAIR: 'pair',
 } as const
 
+/** A convenient way to retreive an actual task from its name. */
 export const TaskMap = {
   [Task.PAIR]: pair,
   [Task.ENCRYPT]: encrypt,
   [Task.DECRYPT]: decrypt,
 } as const
 
+/**
+ * Starts a background task by opening a runtime port. Every background task has
+ * a foreground counterpart that may do nothing.
+ *
+ * @param taskName
+ * @param task
+ * @param initialValue
+ * @param report
+ * @param signal
+ * @returns
+ */
 export const runBackgroundTask = async <T extends keyof typeof TaskMap>(
   taskName: T,
   task: ForegroundTask<typeof TaskMap[T]>,
@@ -132,3 +176,10 @@ export const runBackgroundTask = async <T extends keyof typeof TaskMap>(
     port.postMessage(initialValue)
   })
 }
+
+/** A do-nothing task, that can be used if the task isn't interactive. */
+export const emptyForegroundTask: ForegroundTask<
+  // `never` outlines the fact that `yield` will not be used
+  BackgroundTask<unknown, never, unknown, never>
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+> = async function* () {}
