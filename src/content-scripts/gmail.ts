c9d2ef4374453547080c75f33021a2901d127214
Gautier Ben Aim <benaim.gautier@gmail.com>
2021-07-13 15:41:07 +0200

    refactor: removed duplicate

--- src/content-scripts/gmail.ts
+++ src/content-scripts/gmail.ts
@@ -1,6 +1,5 @@
 import { debug } from 'debug'
 import { Observable } from 'observable'
-import type { Writable } from 'svelte/store'
 import DecryptButton from './DecryptButton.svelte'
 import EncryptButton from './EncryptButton.svelte'
 import {
@@ -12,9 +11,6 @@ import {
   isEncryptedText,
   reporter,
 } from './encryption'
-
-// Enable logging in the page console (not the extension console)
-if (process.env.NODE_ENV !== 'production') debug.enable('*')
 
 /** Selectors for interesting HTML Elements of Gmail. */
 const Selector = {
@@ -57,13 +53,76 @@ const handleMailElement = (mailElement: HTMLElement) => {
   }
 }
 
-/** Adds a button next to the text node given. */
+/** Adds a decryption button next to the text node given. */
 const addDecryptButton = (node: Text, encryptedString: string) => {
   // Add the button right before the beginning of the encrypted content
   const target = document.createElement('span')
   const button = new DecryptButton({ target })
   node.before(target)
 
+  /** Frame containing the decrypted mail. */
+  let frame: HTMLIFrameElement
+
+  addClickListener(button, async (state, signal) => {
+    if (state.get() === ButtonState.DONE) {
+      frame.parentNode?.removeChild(frame)
+      return ButtonState.IDLE
+    }
+
+    state.set(ButtonState.IN_PROGRESS)
+
+    // Decrypt and display
+    const decryptedString = await decryptString(
+      encryptedString,
+      reporter((tooltip: string) => {
+        button.$set({ tooltip })
+      }),
+      signal
+    )
+
+    if (decryptedString && node.parentNode)
+      frame = displayDecryptedMail(decryptedString, node.parentNode)
+
+    return ButtonState.DONE
+  })
+}
+
+/** Adds an encryption button in the toolbar. */
+const handleToolbar = (toolbar: HTMLElement) => {
+  if (FLAG in toolbar.dataset) return
+  toolbar.dataset[FLAG] = '1'
+
+  const button = new EncryptButton({
+    target: toolbar,
+  })
+
+  addClickListener(button, async (state, signal) => {
+    state.set(ButtonState.IN_PROGRESS)
+
+    const mail = toolbar.closest('.iN')?.querySelector('[contenteditable]')
+    if (!mail || !mail.textContent) throw new Error('Please write a mail.')
+
+    // Encrypt and replace
+    mail.textContent = await encryptString(
+      mail.textContent,
+      reporter((tooltip: string) => {
+        button.$set({ tooltip })
+      }),
+      signal
+    )
+
+    return ButtonState.DONE
+  })
+}
+
+/** Adds all the listeners necessary to make the button interactive. */
+const addClickListener = (
+  button: EncryptButton | DecryptButton,
+  listener: (
+    state: Observable<ButtonState>,
+    signal: AbortSignal
+  ) => PromiseLike<ButtonState>
+) => {
   /** Current state of the process. */
   const state = new Observable(ButtonState.IDLE)
   state.subscribe((state) => {
@@ -78,59 +137,30 @@ const addDecryptButton = (node: Text, encryptedString: string) => {
     state.set(ButtonState.IDLE)
   })
 
-  /** Frame containing the decrypted mail. */
-  let frame: HTMLIFrameElement
-
-  // When the button is clicked, start the decryption process
+  // When the button is clicked, trigger the event listener
   button.$on('click', async () => {
-    if (state.get() === ButtonState.DONE) {
-      frame.parentNode?.removeChild(frame)
-      state.set(ButtonState.IDLE)
-      return
-    }
-
     // Prevent the process from running twice
     if (state.get() === ButtonState.IN_PROGRESS) return
-    state.set(ButtonState.IN_PROGRESS)
     controller = new AbortController()
 
-    // Decrypt and display
-    const decryptedString = await startDecryption(
-      encryptedString,
-      state,
-      button,
-      controller.signal
-    )
+    try {
+      // Run the listener
+      const newState = await listener(state, controller.signal)
 
-    if (decryptedString && node.parentNode)
-      frame = displayDecryptedMail(decryptedString, node.parentNode)
+      // Mark the job as done, or any other state returned by the listener
+      state.set(newState)
+      button.$set({ tooltip: undefined })
+    } catch (error: unknown) {
+      if (controller.signal.aborted) return
+
+      // Mark the job as failed, and propagate the error message
+      state.set(ButtonState.FAILED)
+      if (error instanceof Error) button.$set({ tooltip: error.message })
+    }
   })
 }
 
-const startDecryption = async (
-  encryptedString: string,
-  state: Writable<ButtonState>,
-  button: DecryptButton,
-  signal: AbortSignal
-) => {
-  try {
-    const decryptedString = await decryptString(
-      encryptedString,
-      reporter((tooltip: string) => {
-        button.$set({ tooltip })
-      }),
-      signal
-    )
-    state.set(ButtonState.DONE)
-    button.$set({ tooltip: undefined })
-    return decryptedString
-  } catch (error: unknown) {
-    if (signal.aborted) return
-    state.set(ButtonState.FAILED)
-    if (error instanceof Error) button.$set({ tooltip: error.message })
-  }
-}
-
+/** Adds a frame containing a given string. */
 const displayDecryptedMail = (decryptedString: string, parent: ParentNode) => {
   const frame = document.createElement('iframe')
   Object.assign(frame.style, {
@@ -143,70 +173,6 @@ const displayDecryptedMail = (decryptedString: string, parent: ParentNode) => {
   frame.sandbox.value = ''
   parent.append(frame)
   return frame
-}
-
-const handleToolbar = (toolbar: HTMLElement) => {
-  if (FLAG in toolbar.dataset) return
-  toolbar.dataset[FLAG] = '1'
-
-  const button = new EncryptButton({
-    target: toolbar,
-  })
-
-  /** Current state of the process. */
-  const state = new Observable(ButtonState.IDLE)
-  state.subscribe((state) => {
-    // Reflect all changes to the button
-    button.$set({ state })
-  })
-
-  /** Abort controller, bound to a button in the tooltip. */
-  let controller: AbortController
-  button.$on('abort', () => {
-    controller.abort()
-    state.set(ButtonState.IDLE)
-  })
-
-  button.$on('click', async () => {
-    // Prevent the process from running twice
-    if (state.get() === ButtonState.IN_PROGRESS) return
-
-    state.set(ButtonState.IN_PROGRESS)
-    controller = new AbortController()
-
-    const mail = toolbar.closest('.iN')?.querySelector('[contenteditable]')
-
-    if (!mail) return
-
-    void startEncryption(mail, button, controller.signal, state)
-  })
-}
-
-const startEncryption = async (
-  mail: Element,
-  button: EncryptButton,
-  signal: AbortSignal,
-  state: Writable<ButtonState>
-) => {
-  try {
-    // Retrieve the mail content
-    if (!mail.textContent) throw new Error('Please write a mail.')
-
-    // Encrypt and replace
-    mail.textContent = await encryptString(
-      mail.textContent,
-      reporter((tooltip: string) => {
-        button.$set({ tooltip })
-      }),
-      signal
-    )
-
-    state.set(ButtonState.DONE)
-  } catch (error: unknown) {
-    if (signal.aborted) return
-    state.set(ButtonState.FAILED)
-    if (error instanceof Error) button.$set({ tooltip: error.message })
-  }
 }
 
 /**
@@ -236,6 +202,9 @@ const handleMutation = (mutation: MutationRecord) => {
     handleToolbar(target)
   }
 }
+
+// Enable logging in the page console (not the extension console)
+if (process.env.NODE_ENV !== 'production') debug.enable('*')
 
 // Start observing the DOM for changes
 new MutationObserver((mutations) => {
