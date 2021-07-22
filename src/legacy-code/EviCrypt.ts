ec8441cfc3f3004a6c8145bd016c4f88a5212079
Gautier Ben Aim <benaim.gautier@gmail.com>
2021-07-22 13:37:35 +0200

    feat: added encryptFile

--- src/legacy-code/EviCrypt.ts
+++ src/legacy-code/EviCrypt.ts
@@ -1,9 +1,11 @@
 /* eslint-disable unicorn/filename-case */
 import { fromUint8Array, toUint8Array } from 'js-base64'
 import {
+  addJammingSimple,
   addJammingSimpleText,
   AesUtil,
   removeJammingSimpleText,
+  shiftLeft,
 } from './AesUtil'
 import {
   concatUint8Array,
@@ -14,6 +16,11 @@ import {
 } from './utils'
 
 const ID_MESSAGE = new Uint8Array([0, 0, 0, 21])
+
+const ID_FILE = new Uint8Array([0, 0, 0, 22])
+const blockSize = 262_144
+const extension = 'Evi'
+const extensionName = '.' + extension
 
 interface Keys {
   high: Uint8Array
@@ -74,6 +81,78 @@ export class EviCrypt {
     const dec = AES.decryptCTR(iv, salt, this.keys.high, dataText.slice(offset))
 
     return uint8ArrayToUTF8(dec)
+  }
+
+  async encryptFile(file: File): Promise<File> {
+    if (file.name.endsWith(extensionName)) return file
+
+    if (file.name.length > 256) throw new Error('Filename too long')
+
+    // Create a random name for the file, the real name is stored
+    // encrypted at the beginning of the file
+    const name =
+      [...crypto.getRandomValues(new Uint8Array(8))]
+        .map((n) => String.fromCharCode(97 + (n % 26)))
+        .join('') + extensionName
+
+    const uintFileName = new TextEncoder().encode(file.name)
+
+    const reader = new FileReader()
+
+    const iv = random(16)
+    const salt = random(32)
+    const shift = random(1)
+    const jam = shiftLeft(concatUint8Array(iv, salt), shift[0])
+
+    const idKey = (
+      await sha256(
+        concatUint8Array(this.keys.low.slice(0, 20), jam.slice(0, 32))
+      )
+    ).slice(0, 20)
+
+    const AES = new AesUtil(256, 1000)
+    const iv2 = random(16)
+    const salt2 = random(32)
+    const temp0 = AES.encryptCTR(iv, salt, this.keys.high, uintFileName)
+    const temp1 = concatUint8Array(salt2, temp0)
+    const data = concatUint8Array(iv2, temp1)
+
+    const blobParts: BlobPart[] = [
+      ID_FILE,
+      new Uint8Array([shift[0] ^ this.keys.low[24]]),
+      idKey,
+      jam,
+      addJammingSimple(data, this.keys.low.slice(20)),
+    ]
+
+    const buffer = await new Promise<Uint8Array>((resolve, reject) => {
+      reader.addEventListener('load', () => {
+        resolve(new Uint8Array(reader.result as ArrayBuffer))
+      })
+      reader.addEventListener('error', () => {
+        reject(reader.error)
+      })
+      reader.readAsArrayBuffer(file)
+    })
+
+    // Encrypt each block separetely
+    let j = 0
+    while (j * blockSize < buffer.length) {
+      const block = AES.encryptCTR(
+        iv2,
+        salt2,
+        this.keys.high,
+        buffer.slice(j * blockSize, (j + 1) * blockSize)
+      )
+      blobParts.push(block)
+      j++
+    }
+
+    const blob = new Blob(blobParts, {
+      type: 'application/octet-stream; charset=utf-8',
+    })
+
+    return new File([blob], name)
   }
 }
 
