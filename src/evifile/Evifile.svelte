fe33524c737b8ca42ba6a8f8f218285025c6023e
Gautier Ben Aim <benaim.gautier@gmail.com>
2021-07-22 11:57:11 +0200

    feat: added dropzone

--- src/evifile/Evifile.svelte
+++ src/evifile/Evifile.svelte
@@ -1,1 +1,42 @@
+<script lang="ts">
+  import Dropzone from 'dropzone'
+  import { onMount } from 'svelte'
+  Dropzone.autoDiscover = false
+
+  let form: HTMLFormElement
+
+  onMount(() => {
+    let dropzone = new Dropzone(form, {
+      url: 'none',
+      autoProcessQueue: false,
+      addRemoveLinks: true,
+      accept: function (file, done) {
+        // https://stackoverflow.com/questions/33710825/getting-file-contents-when-using-dropzonejs
+        var reader = new FileReader()
+        reader.addEventListener('loadend', function (event) {
+          if (!event.target) {
+            dropzone.emit('error', file)
+            return
+          }
+          console.log(event.target.result)
+          done()
+          dropzone.emit('success', file)
+          dropzone.emit('complete', file)
+        })
+        reader.readAsText(file)
+      },
+    })
+  })
+</script>
+
 <h1>Hello World!</h1>
+
+<form class="dropzone" bind:this={form}>
+  <h2 class="dz-message">Drop files here to decrypt</h2>
+</form>
+
+<style lang="scss">
+  :global {
+    @import './assets/dropzone';
+  }
+</style>
