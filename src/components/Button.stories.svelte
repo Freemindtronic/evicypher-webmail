67e0427daec24c3f729f564ae6cca4b62e712573
Gautier Ben Aim <benaim.gautier@gmail.com>
2021-08-04 12:48:13 +0200

    test: added storybook

---
+++ src/components/Button.stories.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Button from './Button.svelte'
+</script>
+
+<Meta
+  title="Button"
+  component={Button}
+  argTypes={{ onClick: { action: 'clicked' } }}
+/>
+
+<Template let:args>
+  <Button on:click={args.onClick}>{args.label}</Button>
+</Template>
+
+<Story
+  name="Primary"
+  args={{
+    label: 'Button',
+  }}
+/>
---
+++ src/components/Button.svelte
@@ -0,0 +1,30 @@
+<!--
+  @component
+  A button component.
+-->
+
+<button on:click><slot /></button>
+
+<style lang="scss">
+  button {
+    color: $background-color;
+    background-color: $input-color;
+    border: 1px solid $input-color;
+    border-radius: 4px;
+
+    &:hover,
+    &:focus {
+      background-color: scale-color($input-color, $lightness: -10%);
+      border-color: scale-color($input-color, $lightness: -30%);
+    }
+
+    &:focus {
+      outline: 0;
+      box-shadow: 0 1px 3px $input-color, 0 0 1px 1px $background-color;
+    }
+
+    &:active {
+      box-shadow: inset 0 2px 4px scale-color($input-color, $lightness: -50%);
+    }
+  }
+</style>
