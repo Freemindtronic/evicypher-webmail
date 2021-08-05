0484318ca46b7a457b62bc0c1d19191a1163c1f1
Gautier Ben Aim <benaim.gautier@gmail.com>
2021-08-05 16:05:57 +0200

    refactor(component/qrcode): use:action

--- src/components/QRCode.svelte
+++ src/components/QRCode.svelte
@@ -1,24 +1,29 @@
 <script lang="ts">
   import { toCanvas } from 'qrcode'
 
-  let canvas: HTMLCanvasElement
-
   export let scale = 3
   export let size = 21 * scale
 
   export let data: string | undefined
 
-  $: if (canvas && data) {
-    void toCanvas(canvas, data, {
-      margin: 0,
-      scale,
-    })
-  } else if (canvas) {
-    canvas.width = size
-    canvas.height = size
-    canvas.style.width = `${size}px`
-    canvas.style.height = `${size}px`
+  const qr = (canvas: HTMLCanvasElement, data: string | undefined) => {
+    const update = (data: string | undefined) => {
+      if (data) {
+        void toCanvas(canvas, data, {
+          margin: 0,
+          scale,
+        })
+      } else {
+        canvas.width = size
+        canvas.height = size
+        canvas.style.width = `${size}px`
+        canvas.style.height = `${size}px`
+      }
+    }
+
+    update(data)
+    return { update }
   }
 </script>
 
-<canvas bind:this={canvas} />
+<canvas use:qr={data} />
