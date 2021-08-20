96e83c829f05f6fb7d522dc6472c6560782c9313
GauBen <benaim.gautier@gmail.com>
2021-08-20 14:06:30 +0200

    refactor: grouped common outlook and gmail code

--- src/content-scripts/common.ts
+++ src/content-scripts/common.ts
@@ -4,10 +4,32 @@
  * @module
  */
 
-import type { Reporter } from '$/report'
-import type DecryptButton from './DecryptButton.svelte'
-import type EncryptButton from './EncryptButton.svelte'
+import type { Report, Reporter } from '$/report'
+import tippy from 'tippy.js'
+import { browser } from 'webextension-polyfill-ts'
+import { ErrorMessage, ExtensionError } from '$/error'
+import { _ } from '$/i18n'
 import { startBackgroundTask, Task } from '$/task'
+import DecryptButton from './DecryptButton.svelte'
+import EncryptButton from './EncryptButton.svelte'
+
+export interface Selectors {
+  MAIL_CONTENT: string
+  TOOLBAR: string
+  MAIL_EDITOR: string
+  EDITOR_CONTENT: string
+  SEND_BUTTON: string
+}
+
+export type Design = 'gmail' | 'outlook' | undefined
+
+export interface Options {
+  selectors: Selectors
+  design: Design
+}
+
+/** A flag to mark already processed (having buttons added) HTML elements. */
+const FLAG = 'freemindtronic'
 
 /** Sends a request to the background script to encrypt the given string. */
 export const encryptString = async (
@@ -103,5 +125,194 @@ export const addClickListener = (
       .catch(() => {
         rejected = true
       })
+  })
+}
+
+/** Adds a button to a given element to decrypt all encrypted parts found. */
+const handleMailElement = (mailElement: HTMLElement, options: Options) => {
+  // Mark the element
+  if (FLAG in mailElement.dataset) return
+  mailElement.dataset[FLAG] = '1'
+
+  // If it's not an encrypted mail, ignore it
+  const mailString = mailElement.textContent
+  if (!mailString || !containsEncryptedText(mailString)) return
+
+  // Find all encrypted parts
+  const treeWalker = document.createTreeWalker(
+    mailElement,
+    NodeFilter.SHOW_TEXT,
+    {
+      acceptNode: (textNode: Text) =>
+        isEncryptedText(textNode.data)
+          ? NodeFilter.FILTER_ACCEPT
+          : NodeFilter.FILTER_SKIP,
+    }
+  )
+
+  let node: Node | null
+  while ((node = treeWalker.nextNode())) {
+    // Add a "Decrypt" button next to the node
+    if (!node.parentNode?.textContent) continue
+    const encryptedString = extractEncryptedString(node.parentNode.textContent)
+    addDecryptButton(node as Text, encryptedString, options)
+  }
+}
+
+/** Adds a decryption button next to the text node given. */
+const addDecryptButton = (
+  node: Text,
+  encryptedString: string,
+  { design }: Options
+) => {
+  // Add the button right before the beginning of the encrypted content
+  const target = document.createElement('span')
+  const button = new DecryptButton({
+    target,
+    props: {
+      report: undefined,
+      promise: undefined,
+      design,
+    },
+  })
+  node.before(target)
+
+  /** Frame containing the decrypted mail. */
+  let frame: HTMLIFrameElement
+
+  addClickListener(button, (promise, resolved, rejected, signal) => {
+    if (resolved) {
+      frame.parentNode?.removeChild(frame)
+      return
+    }
+
+    if (promise && !rejected) return promise
+
+    button.$set({ report: undefined })
+
+    // Decrypt and display
+    return decryptString(
+      encryptedString,
+      (report: Report) => {
+        button.$set({ report })
+      },
+      signal
+    ).then((decryptedString) => {
+      frame = displayDecryptedMail(
+        decryptedString,
+        node.parentNode as ParentNode
+      )
+    })
+  })
+}
+
+/** Adds an encryption button in the toolbar. */
+const handleToolbar = (
+  toolbar: HTMLElement,
+  { selectors, design }: Options
+) => {
+  if (FLAG in toolbar.dataset) return
+  toolbar.dataset[FLAG] = '1'
+
+  const editor = toolbar.closest(selectors.MAIL_EDITOR)
+  const mail = editor?.querySelector(selectors.EDITOR_CONTENT)
+  const sendButton = editor?.querySelector<HTMLElement>(selectors.SEND_BUTTON)
+  if (!editor || !mail || !sendButton) return
+
+  const button = new EncryptButton({
+    target: toolbar,
+    props: {
+      report: undefined,
+      promise: undefined,
+      design,
+    },
+  })
+
+  const tooltip = tippy(sendButton, {
+    theme: 'light-border',
+  })
+  _.subscribe(($_) => {
+    tooltip.setContent($_('this-mail-is-not-encrypted'))
+  })
+
+  addClickListener(button, async (promise, resolved, rejected, signal) => {
+    if (promise && !resolved && !rejected) return promise
+
+    if (!mail.textContent)
+      throw new ExtensionError(ErrorMessage.MAIL_CONTENT_UNDEFINED)
+
+    button.$set({ report: undefined })
+
+    // Encrypt and replace
+    return encryptString(
+      // Use innerHTML instead of textContent to support rich text
+      mail.innerHTML,
+      (report: Report) => {
+        button.$set({ report })
+      },
+      signal
+    ).then((encryptedString) => {
+      mail.textContent = encryptedString
+      tooltip.destroy()
+    })
+  })
+}
+
+/** Adds a frame containing a given string. */
+const displayDecryptedMail = (decryptedString: string, parent: ParentNode) => {
+  const frame = document.createElement('iframe')
+  Object.assign(frame.style, {
+    display: 'block',
+    width: '100%',
+    maxWidth: '100%',
+    boxSizing: 'border-box',
+  })
+
+  // To address issues with Content-Security-Policy,
+  // we need a local frame, that we modify once loaded
+  frame.src = browser.runtime.getURL('/blank.html')
+
+  parent.append(frame)
+  frame.addEventListener('load', () => {
+    if (!frame.contentDocument) throw new Error('Cannot change frame content.')
+    // We are injecting raw HTML in a sandboxed environnement,
+    // no need to sanitize it
+    // eslint-disable-next-line no-unsanitized/property
+    frame.contentDocument.body.innerHTML = decryptedString
+    // Make the frame as tall as its content
+    frame.height = `${frame.contentDocument.body.scrollHeight + 20}`
+  })
+
+  return frame
+}
+
+/**
+ * Handles mutations observed by the `MutationObserver` below, i.e.
+ * notifications of elements added or removed from the page.
+ */
+const handleMutations = (options: Options) => {
+  // The user opens a mail
+  const mails = document.body.querySelectorAll<HTMLElement>(
+    options.selectors.MAIL_CONTENT
+  )
+  for (const mail of mails) handleMailElement(mail, options)
+
+  // The user starts writing a mail
+  const toolbars = document.body.querySelectorAll<HTMLElement>(
+    options.selectors.TOOLBAR
+  )
+  for (const toolbar of toolbars) handleToolbar(toolbar, options)
+}
+
+/** Observes the DOM for changes. Should work for most webmails. */
+export const observe = (options: Options): void => {
+  // Run the listener on page load
+  handleMutations(options)
+  // Start observing the DOM for changes
+  new MutationObserver(() => {
+    handleMutations(options)
+  }).observe(document.body, {
+    subtree: true,
+    childList: true,
   })
 }
--- src/content-scripts/outlook.ts
+++ src/content-scripts/outlook.ts
@@ -4,192 +4,19 @@
  * @module
  */
 
-import type { Report } from '$/report'
 import { debug } from 'debug'
-import { browser } from 'webextension-polyfill-ts'
-import { ErrorMessage, ExtensionError } from '$/error'
-import DecryptButton from './DecryptButton.svelte'
-import EncryptButton from './EncryptButton.svelte'
-import {
-  addClickListener,
-  containsEncryptedText,
-  decryptString,
-  encryptString,
-  extractEncryptedString,
-  isEncryptedText,
-} from './common'
+import { observe } from './common'
 
 /** Selectors for interesting HTML Elements of Outlook. */
-const Selector = {
+const selectors = {
   MAIL_CONTENT: '.QMubUjbS-BOly_BTHEZj7',
   TOOLBAR: '._2ELnTBajF7jzj_m_hoj3Xt',
   MAIL_EDITOR: '._17WvdmDfhREFqBNvlLv75X',
   EDITOR_CONTENT: '[contenteditable]',
-}
-
-/** A flag to mark already processed (having buttons added) HTML elements. */
-const FLAG = 'freemindtronicButtonAdded'
-
-/** Adds a button to a given element to decrypt all encrypted parts found. */
-const handleMailElement = (mailElement: HTMLElement) => {
-  // Mark the element
-  if (FLAG in mailElement.dataset) return
-  mailElement.dataset[FLAG] = '1'
-
-  // If it's not an encrypted mail, ignore it
-  const mailString = mailElement.textContent
-  if (!mailString || !containsEncryptedText(mailString)) return
-
-  // Find all encrypted parts
-  const treeWalker = document.createTreeWalker(
-    mailElement,
-    NodeFilter.SHOW_TEXT,
-    {
-      acceptNode: (textNode: Text) =>
-        isEncryptedText(textNode.data)
-          ? NodeFilter.FILTER_ACCEPT
-          : NodeFilter.FILTER_SKIP,
-    }
-  )
-
-  let node: Node | null
-  while ((node = treeWalker.nextNode())) {
-    // Add a "Decrypt" button next to the node
-    if (!node.parentNode?.textContent) continue
-    const encryptedString = extractEncryptedString(node.parentNode.textContent)
-    addDecryptButton(node as Text, encryptedString)
-  }
-}
-
-/** Adds a decryption button next to the text node given. */
-const addDecryptButton = (node: Text, encryptedString: string) => {
-  // Add the button right before the beginning of the encrypted content
-  const target = document.createElement('span')
-  const button = new DecryptButton({
-    target,
-    props: {
-      report: undefined,
-      promise: undefined,
-      design: 'outlook',
-    },
-  })
-  node.before(target)
-
-  /** Frame containing the decrypted mail. */
-  let frame: HTMLIFrameElement
-
-  addClickListener(button, (promise, resolved, rejected, signal) => {
-    if (resolved) {
-      frame.parentNode?.removeChild(frame)
-      return
-    }
-
-    if (promise && !rejected) return promise
-
-    button.$set({ report: undefined })
-
-    // Decrypt and display
-    return decryptString(
-      encryptedString,
-      (report: Report) => {
-        button.$set({ report })
-      },
-      signal
-    ).then((decryptedString) => {
-      frame = displayDecryptedMail(
-        decryptedString,
-        node.parentNode as ParentNode
-      )
-    })
-  })
-}
-
-/** Adds an encryption button in the toolbar. */
-const handleToolbar = (toolbar: HTMLElement) => {
-  if (FLAG in toolbar.dataset) return
-  toolbar.dataset[FLAG] = '1'
-
-  const button = new EncryptButton({
-    target: toolbar,
-    props: {
-      report: undefined,
-      promise: undefined,
-      design: 'outlook',
-    },
-  })
-
-  addClickListener(button, async (promise, resolved, rejected, signal) => {
-    if (promise && !resolved && !rejected) return promise
-
-    const mail = toolbar
-      .closest(Selector.MAIL_EDITOR)
-      ?.querySelector(Selector.EDITOR_CONTENT)
-    if (!mail || !mail.textContent)
-      throw new ExtensionError(ErrorMessage.MAIL_CONTENT_UNDEFINED)
-
-    button.$set({ report: undefined })
-
-    // Encrypt and replace
-    return encryptString(
-      // Use innerHTML instead of textContent to support rich text
-      mail.innerHTML,
-      (report: Report) => {
-        button.$set({ report })
-      },
-      signal
-    ).then((encryptedString) => {
-      mail.textContent = encryptedString
-    })
-  })
-}
-
-/** Adds a frame containing a given string. */
-const displayDecryptedMail = (decryptedString: string, parent: ParentNode) => {
-  const frame = document.createElement('iframe')
-  Object.assign(frame.style, {
-    display: 'block',
-    width: '100%',
-    maxWidth: '100%',
-    boxSizing: 'border-box',
-  })
-
-  frame.src = browser.runtime.getURL('/blank.html')
-
-  parent.append(frame)
-  frame.addEventListener('load', () => {
-    if (!frame.contentDocument) throw new Error('Cannot change frame content.')
-    // We are injecting raw HTML in a sandboxed environnement,
-    // no need to sanitize it
-    // eslint-disable-next-line no-unsanitized/property
-    frame.contentDocument.body.innerHTML = decryptedString
-    // Make the frame as tall as its content
-    frame.height = `${frame.contentDocument.body.scrollHeight + 20}`
-  })
-
-  return frame
-}
-
-/**
- * Handles mutations observed by the `MutationObserver` below, i.e.
- * notifications of elements added or removed from the page.
- */
-const handleMutations = () => {
-  // The user opens a mail
-  const mails = document.body.querySelectorAll<HTMLElement>(
-    Selector.MAIL_CONTENT
-  )
-  for (const mail of mails) handleMailElement(mail)
-
-  // The user starts writing a mail
-  const toolbars = document.body.querySelectorAll<HTMLElement>(Selector.TOOLBAR)
-  for (const toolbar of toolbars) handleToolbar(toolbar)
+  SEND_BUTTON: '.css-373',
 }
 
 // Enable logging in the page console (not the extension console)
 if (process.env.NODE_ENV !== 'production') debug.enable('*')
 
-// Start observing the DOM for changes
-new MutationObserver(handleMutations).observe(document.body, {
-  subtree: true,
-  childList: true,
-})
+observe({ selectors, design: 'outlook' })
