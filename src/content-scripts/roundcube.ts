d867435fc2d7b64d2ce7e74ce2b6e5fc7b2778e0
TheLordDuck <thelordduck22@gmail.com>
2021-08-31 11:24:39 +0200

    feat: added support for roundcube-ovh

---
+++ src/content-scripts/roundcube.ts
@@ -0,0 +1,24 @@
+/**
+ * Roundcube content script.
+ *
+ * @module
+ */
+
+import { debug } from 'debug'
+import { observe, Selectors } from './common'
+import { Design } from './design'
+
+/** Selectors for interesting HTML Elements of Roundcube. */
+const selectors: Selectors = {
+  mail: '._rp_U4 > :first-child',
+  toolbar: '._mcp_H2',
+  editor: '._mcp_e1',
+  editorContent: '[contenteditable]',
+  send: '[autoid=_mcp_g]',
+  encryptButtonSibling: '[autoid=_mcp_h]',
+}
+
+// Enable logging in the page console (not the extension console)
+if (process.env.NODE_ENV !== 'production') debug.enable('*')
+
+observe({ selectors, design: Design.OldOutlook })
