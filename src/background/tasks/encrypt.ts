4ba6a919b87d1b32af970bffb07693907ad645bf
Gautier Ben Aim <benaim.gautier@gmail.com>
2021-09-02 16:04:50 +0200

    feat: fetch and convert keys

--- src/background/tasks/encrypt.ts
+++ src/background/tasks/encrypt.ts
@@ -6,12 +6,13 @@
  *
  * @module
  */
+
 import type { BackgroundTask } from '$/task'
 import debug from 'debug'
+import { fromUint8Array } from 'js-base64'
 import {
   createMessage,
-  readKey,
-  encrypt as encryptText,
+  encrypt as openpgpEncrypt,
   config as openpgpConfig,
 } from 'openpgp'
 import { get } from 'svelte/store'
@@ -36,59 +37,26 @@ import { version } from '~/package.json'
  * @returns The encrypted string
  */
 export const encrypt: BackgroundTask<undefined, string, string> =
-  async function* (_context, _reporter, _signal) {
+  async function* (context, reporter, signal) {
     const text = yield
 
-    const message = await createMessage({ text })
+    await BrowserStore.allLoaded
 
-    const encryptionKeys = await readKey({
-      armoredKey: `-----BEGIN PGP PUBLIC KEY BLOCK-----
+    // Fetch the cerificate of the favorite phone in browser storage
+    const phone = get(favoritePhone)
 
-mQGNBGEk0u0BDAC91yw37xKn04aTNeakDf8ywfTh9m4qyx3si9YRKTBQxyw2fEFw
-vOoocxzPZ0burcwbde+JWM02yGULwDF1hOrwY4Ig4USbVoxTQUMUiX0KiJVJxxKY
-DeFwAYFhX/lGyR+6bCVa1INzH5tmnth6RivrTEwQQf41Oo6nMCm2YL6tT1AkQhp8
-K5V9xw5Dx8t/CLlXOwIDkRqdgdgTaHE1g0Kv2Ygpo0P33jZahbBmJU0V7GHU0ugP
-W/ZvS7pgdoo478qwj/vtQ7LglLEoWzLSkQfc/XO8gj1HKihW1s5U70CmNylBX8u8
-RPtYOhNMvTrJxIuJU7xDSd9xDBv0GGBnFCnE0+6xH8laL3A2cbmeyxeNiofPFity
-TdGIIl+UJ2PVFssbAEotbTf8/TX7oKPkTtP6NyjYwAsaz+fso9mN9ek9X0SrVKMG
-R6ScoJ4tUIsAsNThUXGHlECExJSbc8ij4brry7Dxhd42oNGdYTyZ2iZ1Xp1EpL5l
-JOV7LUotny4lTGkAEQEAAbQ6RXZpQ3lwaGVyIFdlYm1haWwgPGV2aWN5cGhlci13
-ZWJtYWlsLXRlc3QtZ3BnQGV4YW1wbGUuY29tPokB1AQTAQoAPhYhBGSbx67BCXtx
-pVkCWj2eXKr3ZzxnBQJhJNLtAhsDBQkDwmcABQsJCAcCBhUKCQgLAgQWAgMBAh4B
-AheAAAoJED2eXKr3Zzxn4pEL/RW3HYmZ2u2N5+QI4xhyS1FSzb2EtLJnIPiLdQix
-6JJhIg95Lm8Y2oxtpOOf81I5mGgCJ0GyT//2FwZh+76zjPizB1m1yMEPDDTOVxnk
-BkhTIwxie38Dsehxb84/hPk/CA4Bl91ymxYDFgUTSG0XQPj1cX9ctc1j4t35zM6O
-L46xRi4X7H/DyQQhxJj/2n01cBClA6p1I7MSJ8imU28mRG1nLsTFV4OQgMdJH8jz
-oFFkY7+h0GJoWA4ezRy9pUvI70jkjY+CgdDwtPQk9JAnyguGofcKvQA9xtErGEBC
-6IDMR+ORzbf4oYTkhU15VkEFFSvfk9DXkK9dq5CoS7j7kyhARwGzvZgPVDrHPDhd
-iR2aGxekdGmppv9ZSGGpBJh5BUYQl+YePX/Jj5NH4twJnUDFn5qliWJBiw3BlZvJ
-rTLmdsWy+j9cCySvzwy5rg4k3JxCO2v64Hyt90QclaEoS3oN8tFwSJuz6eVfFJNQ
-hocRNJok67s0yTm2eR/tA+HANrkBjQRhJNLtAQwAwHyQo36vjCZMoqDXdsUVYTNQ
-NEWADSL1aQFyU0rGHo7W4D+waJ9jEVSsrovJE9P6/vFcSQawlW5O3P9QQoqmL4mr
-PP+3BWdys0XWmcjBmXCEZbEgVlf+q4k85egk5rvfRaKgu6GK/8b5L1YKitVABsA/
-FMvw4oUIY3iKWp86NCqYLP5ZY/1m+FnGOesUab866NvfuULZtqtGT0PMeEkjDPFE
-7JJdUrdH3oWbPnekRh/+ZLHI11qq5yyW0hHTDxoHbbLF5C+McNaS30Kju4cQY9cL
-KXA/VGZuxZp36y7qXN5Gc0i+lWapMC/wXOTCe2AZAvxpIfLT8DaMTONSCaOvnv5R
-GRpxtUjAusitfXRrfDHS7SnXkmPxMzrCNU0wbuRa7OmFwUmiR4q9vxrnTC4WOfEt
-oXapDJ1UdS7o7KUeloPdMyCnQmkRwjDsrtJ3XebM1NppWZu1L9ygROuwQJhGkjwG
-TxXS6V6shTDI7fsdwJVqBsopTe2GoCTjDCJeFo0RABEBAAGJAbwEGAEKACYWIQRk
-m8euwQl7caVZAlo9nlyq92c8ZwUCYSTS7QIbDAUJA8JnAAAKCRA9nlyq92c8Z1Qg
-C/9GQ5PL0X2kb9ahfd5mdNruzdWb8lkhuWwjsHjHZqCCl+e3lhXHTgsFUB046ujW
-C9Ick9wDW9icmAxUFoQb0jTB+qfKKXxTnQEpCwJ97d6AkBtQYQUyCKx/uqY7QOnh
-YDKbaoHJI6l/q2CfNnyvoytDN/12wAxSAb+lxxXRul66v+EInTgcwh+b+4ML7w+z
-UoxPHqGaFtVYCe3S1LR888oucpT2s5io3L2zKX9feFkcW2wW9GKJekelHthX75zc
-TPUQQQaDwtt4gUVjN7SoMUN+5qG8yLD+igx7z5fEVbDME2WFDiii3/1I5hdXq1dN
-2KstVk8BbePHg7qQ1OQpylTF4TmIJwdFWlfS5l1Wv9i3viS+X17LWS2cWELNVWPJ
-Jbsr6oZqlbR3kkIj/+epkA/tZZ9lXDTU29DvgpRbq55w0d6ElJs3NdwQ13YYcG3K
-l2jC4/9YUhIl2sINlpgOeWTljcHTDhkSho9dVGXnlnVcu+64ysZ2+V08koUUhWbn
-Q4A=
-=GABk
------END PGP PUBLIC KEY BLOCK-----`,
+    if (phone === undefined)
+      throw new ExtensionError(ErrorMessage.FavoritePhoneUndefined)
+
+    // Send a request to the FMT app
+    const keys = await fetchAndSaveKeys(context, phone, {
+      reporter,
+      signal,
     })
 
-    return encryptText({
-      message,
-      encryptionKeys,
+    return openpgpEncrypt({
+      message: await createMessage({ text }),
+      passwords: fromUint8Array(keys.high),
       config: {
         showVersion: true,
         versionString: `EviCypher Webmail ${version} (${openpgpConfig.versionString})`,
