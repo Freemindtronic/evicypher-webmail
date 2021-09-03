95e5a233e4afdabe6cedf470f3e3b0e00d143dba
Gautier Ben Aim <benaim.gautier@gmail.com>
2021-09-03 13:01:36 +0200

    chore: added locales as web accessible resources

--- manifest.js
+++ manifest.js
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable camelcase */
 import { readFileSync, writeFileSync } from 'fs'
 import { fileURLToPath } from 'url'
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const webmails = JSON.parse(readFileSync('./webmails.json'))
 
 /** A dynamically generated manifest, to keep the version number consistent. */
@@ -24,7 +25,6 @@ export const manifest = {
     default_popup: 'popup.html',
   },
   content_scripts: Object.entries(webmails).map(([key, matches]) => ({
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     matches,
     js: [`content-script-${key}.js`],
   })),
@@ -33,14 +33,28 @@ export const manifest = {
       id: 'evicypher-webmail@freemindtronic.com',
     },
   },
-  web_accessible_resources: ['loading.gif', 'blank.html'],
+  web_accessible_resources: ['loading.gif', 'blank.html', 'locales/*.json'],
 }
 
-/** Writes `manifest` to `extension/manifest.json`. */
+/** Writes `manifest` to `build/manifest.json`. */
 export const writeManifest = () => {
   writeFileSync('./build/manifest.json', JSON.stringify(manifest, undefined, 2))
   console.log('\u001B[32m%s\u001B[0m', `created build/manifest.json`)
 }
 
+/** Patches `extension/manifest.json`. */
+export const patchManifest = () => {
+  const patchedManifest = JSON.parse(readFileSync('./extension/manifest.json'))
+  patchedManifest.web_accessible_resources = manifest.web_accessible_resources
+  writeFileSync(
+    './extension/manifest.json',
+    JSON.stringify(patchedManifest, undefined, 2)
+  )
+  console.log('\u001B[32m%s\u001B[0m', `patched extension/manifest.json`)
+}
+
 // Save the manifest if the file is run directly
-if (process.argv[1] === fileURLToPath(import.meta.url)) writeManifest()
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  if (process.argv[2] === '--patch') patchManifest()
+  else writeManifest()
+}
