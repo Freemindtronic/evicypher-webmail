e2a3cbd95cde0a786a2016ef9850e09804cd5c92
Dylan Da Costa <dylandcf23@gmail.com>
2021-09-15 16:06:26 +0200

    feat: qrcode implementation

---
+++ src/content-scripts/QRCodeButton.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import type { Report } from '$/report'
+  import { _ } from '$/i18n'
+  import Button from './TaskButton.svelte'
+  import QRIdle from './assets/qr.svg'
+  import { Design } from './design'
+
+  /** Tooltip content. */
+  export let report: Report | undefined = undefined
+
+  /** A promise for the state of the process. */
+  export let promise: Promise<void> | undefined = undefined
+
+  /** Design option. */
+  export let design: Design = Design.None
+</script>
+
+<!-- Svelte favors composition over inheritence, so this is the "Svelte-way" of recycling components -->
+<Button
+  bind:report
+  bind:promise
+  {design}
+  tooltipPlacement="top-start"
+  IdleIcon={QRIdle}
+  idleTooltip={$_('scan-the-qr-code-with-the-application')}
+  doneTooltip={$_('click-to-close-the-qr-code')}
+  task="qrcode"
+  on:click
+  on:abort
+>
+  {$_('qr-code')}
+</Button>
