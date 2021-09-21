7f9b898cb8d25732795029d6207d83a06a599f7b
Dylan Da Costa <dylandcf23@gmail.com>
2021-09-21 08:57:13 +0200

    feat: add option between OpenPGP anf legacy encryption

---
+++ src/components/Switch.svelte
@@ -0,0 +1,62 @@
+<script>
+  export let checked = false
+
+  export const color = '#2196F3'
+</script>
+
+<label class="switch">
+  <input type="checkbox" bind:checked />
+  <span class="slider" />
+</label>
+
+<style lang="scss">
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 50px;
+    height: 20px;
+  }
+
+  .switch input {
+    width: 0;
+    height: 0;
+    opacity: 0;
+  }
+
+  .slider {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: #ccc;
+    border-radius: 20px;
+    cursor: pointer;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+
+  .slider:before {
+    position: absolute;
+    bottom: 2px;
+    left: 4px;
+    width: 17px;
+    height: 17px;
+    background-color: white;
+    border-radius: 50%;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+    content: '';
+  }
+
+  input:checked + .slider {
+    background-color: $input-color;
+    box-shadow: 0 0 1px $input-color;
+  }
+
+  input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
+</style>
---
+++ src/options.ts
@@ -0,0 +1,16 @@
+/**
+ * Extension options
+ *
+ * Contains all the options of the extension
+ *
+ * @module
+ */
+
+import { writable } from 'svelte/store'
+import { BrowserStore } from '$/browser-store'
+
+/** Store either to use OpenPGP or Legacy encryption */
+export const isOpenpgpEnabled: BrowserStore<boolean> = new BrowserStore(
+  'openpgpEnabled',
+  writable(true)
+)
