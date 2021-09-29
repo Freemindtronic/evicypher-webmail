82e9231accc8c378a5fb6f4b0684ef56115ea8b9
TheLordDuck <thelordduck22@gmail.com>
2021-09-29 16:22:05 +0200

    fix: yahoo webmail selector change

--- src/content-scripts/yahoo.ts
+++ src/content-scripts/yahoo.ts
@@ -14,9 +14,8 @@ const selectors: Selectors = {
   toolbar: '.z_Z14vXdP.D_F.ab_C.I_52qC.W_6D6F.p_R.B_0',
   editor: '.em_N.D_F.ek_BB.p_R.o_h',
   editorContent: '[contenteditable]',
-  send: '.q_Z2aVTcY.e_dRA.k_w.r_P.H_6VdP.s_3mS2U.en_0.M_1gLo4F.V_M.cZ1RN91d_n.y_Z2hYGcu.A_6EqO.u_e69.b_0.C_52qC.I4_Z29WjXl.ir3_1JO2M7.it3_dRA',
-  encryptButtonSibling:
-    '.q_Z2aVTcY.e_dRA.k_w.r_P.H_6VdP.s_3mS2U.en_0.M_1gLo4F.V_M.cZ1RN91d_n.y_Z2hYGcu.A_6EqO.u_e69.b_0.C_52qC.I4_Z29WjXl.ir3_1JO2M7.it3_dRA',
+  send: '.q_Z2aVTcY.e_dRA.k_w.r_P',
+  encryptButtonSibling: '.q_Z2aVTcY.e_dRA.k_w.r_P',
 }
 
 // Enable logging in the page console (not the extension console)
