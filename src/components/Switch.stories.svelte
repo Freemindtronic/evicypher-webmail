3387938366f9239e32e99cffcaeb74d9d75634f3
Francois <francois@freemindtronic.com>
2021-10-25 10:56:43 +0200

    test: add switch to storybook

---
+++ src/components/Switch.stories.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Switch from './Switch.svelte'
+</script>
+
+<Meta title="Components/Switch" component={Switch} />
+
+<Template>
+  <Switch />
+</Template>
+
+<Story name="Standard usage" />
