dbb3f8143460b8d26955d1f7de7c9764cef0878f
Dylan Da Costa <dylandcf.dev@gmail.com>
2021-11-18 16:58:25 +0100

    wip: error with encryption

---
+++ src/content-scripts/skype.ts
@@ -0,0 +1,27 @@
+/**
+ * Skype functions for content scripts.
+ *
+ * @module
+ */
+
+import { debug } from 'debug'
+import { Design } from './design'
+import { Webmail, Selectors } from './webmail'
+
+/** Selectors for interesting HTML Elements of Skype. */
+const selectors: Selectors = {
+  mail: '[role="region"]',
+  toolbar:
+    '.noFocusOutline > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)',
+  editor:
+    '.noFocusOutline > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)',
+  send: '.noFocusOutline > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > button:nth-child(1)',
+  editorContent:
+    '.public-DraftStyleDefault-block > span:nth-child(1) > span:nth-child(1)',
+}
+
+// Enable logging in the page console (not the extension console)
+if (process.env.NODE_ENV !== 'production') debug.enable('*')
+
+const webmail = new Webmail(selectors, Design.WhatsApp)
+webmail.observe()
