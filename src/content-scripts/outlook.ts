40b037b6dcf3a05502dbc601af3e675cae83c024
Dylan Da Costa <dylandcf.dev@gmail.com>
2021-11-23 08:38:16 +0100

    fix: send button class

--- src/content-scripts/outlook.ts
+++ src/content-scripts/outlook.ts
@@ -14,7 +14,7 @@ const selectors: Selectors = {
   toolbar: '._2ELnTBajF7jzj_m_hoj3Xt',
   editor: '._17WvdmDfhREFqBNvlLv75X',
   editorContent: '[contenteditable]',
-  send: '._3BlbI7rjg2J-d7fY98r7tp',
+  send: '.ms-Button--primary',
 }
 
 // Enable logging in the page console (not the extension console)
