15efec614aabbf546ccde5481f3f9dbee1b4566e
Dylan <dylandcf.dev@gmail.com>
2023-01-23 10:00:48 +0100

    feat: add pwned password check

---
+++ src/content-scripts/pwned-pasword.ts
@@ -0,0 +1,60 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable new-cap */
+import axios from 'axios'
+import SHA1 from 'crypto-js/sha1.js'
+import {
+  corruptedIcon,
+  corruptedIconFocus,
+  noCorruptedIcon,
+  noCorruptedIconFocus,
+} from './assets/base64/autofill-icons'
+
+/**
+ * The images object is an object literal that has two properties: safe and
+ * unsafe. Each of them has a value that is another object with two properties:
+ * image and imageFocus. Their values represent base64 encoded image. Used to
+ * indicate the security status of a password
+ */
+export const images = {
+  safe: { image: noCorruptedIcon, imageFocus: noCorruptedIconFocus },
+  unsafe: { image: corruptedIcon, imageFocus: corruptedIconFocus },
+}
+
+/**
+ * Takes input field an determines whether the isSafe variable is true or false
+ * and assigns a set of images to the imageSet variable.
+ *
+ * @param field
+ */
+export const setImage = (
+  field: HTMLInputElement,
+  icon: { focus: string; blur: string },
+  isSafe: boolean | undefined
+) => {
+  const imageSet = isSafe ? images.safe : images.unsafe
+  field.style.backgroundImage = `url(data:image/svg+xml;base64,${imageSet.image})`
+  icon.focus = `url(data:image/svg+xml;base64,${imageSet.imageFocus})`
+  icon.blur = `url(data:image/svg+xml;base64,${imageSet.image})`
+}
+
+/**
+ * Check if the password has been made public in a data breach.
+ *
+ * @param password Password to be checked
+ */
+export const checkPassword = async (
+  password: string | CryptoJS.lib.WordArray
+) => {
+  const hash = SHA1(password).toString()
+  const prefix = hash.slice(0, 5)
+  const suffix = hash.slice(5)
+
+  try {
+    const response = await axios.get(
+      `https://api.pwnedpasswords.com/range/${prefix}`
+    )
+    return !response.data.includes(suffix.toUpperCase())
+  } catch (error: unknown) {
+    console.log('Error:', error)
+  }
+}
